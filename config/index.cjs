3f0c823430617716a40e5e3c5c87a813c45217aa
2529179332 <94233873+2529179332@users.noreply.github.com>
2024-12-24 20:07:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小欢',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '4b1f3e0177f1476cafd22f74354af7a3',
+      id: 'SCT94875TrF4sq7d9jWMGIPJop9J8OlBR',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
